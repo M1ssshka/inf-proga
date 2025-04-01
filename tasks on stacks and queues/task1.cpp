060282dfaf0b3fda7ec741bd604938c63d29023d
Mikhail Slepov <anim66056@gmail.com>
2025-04-01 20:43:50 +0400

    feat(stack): implement stack structure with push, pop and reverse - Created `stack` structure - Added `push()` to insert elements - Added `pop()` to remove elements - Implemented `reverse()` to invert stack order

--- tasks on stacks and queues/task1.cpp
+++ tasks on stacks and queues/task1.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+struct stack{
+    int inf;
+    stack *next;
+};
+
+void push(stack *&h, int x) {
+    stack *r = new stack;
+    r->inf = x;
+    r->next = h;
+    h = r;
+}
+
+int pop(stack *&h) {
+    int i = h->inf;
+    stack *r = h;
+    h = h->next;
+    delete r;
+    return i;
+}
+
+void reverse(stack *&h) {
+    stack *head1 = NULL;
+    while(h) {
+        push(head1, pop(h));
+    }
+    h = head1;
+}
+
 int main() {
     
 }
