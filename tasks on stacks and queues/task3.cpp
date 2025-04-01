00b5786f4fe0dd20f9f3a64f52f3c0ff63f52b25
Mikhail Slepov <anim66056@gmail.com>
2025-04-01 21:13:42 +0400

    feat(queue): implemented cyclic shift until first even element

--- tasks on stacks and queues/task3.cpp
+++ tasks on stacks and queues/task3.cpp
@@ -31,5 +31,40 @@ int pop(queue *&h, queue *&t) {
 }
 
 int main() {
-
+    int n;
+    cin >> n;
+    queue *head = NULL;
+    queue *tail = NULL;
+    queue *head1 = NULL;
+    queue *tail1 = NULL;
+    queue *resh = NULL;
+    queue *rest = NULL; 
+    int first_chet = 0;
+    for (int i = 0; i < n; i++) {
+        int t; 
+        cin >> t;
+        if (t % 2 == 0 && !first_chet) 
+            first_chet = t;
+        push(head, tail, t);
+    }
+    for (int i = 0; i < n; i++) {
+        int t = pop(head, tail);
+        if (t == first_chet) {
+            push(resh, rest, t);
+            break;
+        }
+        else 
+            push(head1, tail1, t);
+    }   
+    while(tail) {
+        int t = pop(head, tail);
+        push(resh, rest, t);
+    }
+    while(tail1) {
+        int t = pop(head1, tail1);
+        push(resh, rest, t);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << pop(resh, rest) << ' ';
+    }
 }
