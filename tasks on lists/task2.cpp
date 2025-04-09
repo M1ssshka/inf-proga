576a17acb6ff3dd6501c1ae8ed9d7443ad2eefee
Mikhail Slepov <anim66056@gmail.com>
2025-04-09 16:21:27 +0400

    feat: implement doubly linked list with basic operations Added complete implementation of a doubly linked list with the following functions: - push() - adds element to the end of list - print() - outputs list elements - find() - searches for element in list - insert_after() - inserts element after specified node - del_node() - deletes specified node - del_list() - clears entire list

---
+++ tasks on lists/task2.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+struct list {
+    int inf;
+    list *prev;
+    list *next;
+};
+
+void push(list *&h, list *&t, int x) {
+    list *r = new list;
+    r->inf = x;
+    r->next = nullptr;
+    if (!h && !t) {
+        r->prev = nullptr;
+        h = r;
+    }
+    else {
+        t->next = r;
+        r->prev = t;
+    }
+    t = r;
+}
+
+void print(list *&h, list *&t) {
+    list *p = h;
+    while(p) {
+        cout << p->inf << " ";
+        p = p->next;
+    }
+    cout << '\n';
+}
+
+list *find(list *&h, list *&t, int x) {
+    list *p = h;
+    while(p) {
+        if (p->inf == x) break;
+        p = p->next;
+    }
+    return p;
+}
+
+void insert_after(list *&h, list *&t, list *&r, int x) {
+    list *p = new list;
+    p->inf = x;
+    if (r == t) {
+        p->next = nullptr;
+        p->prev = r;
+        r->next = p;
+        t = p;
+    }
+    else {
+        r->next->prev = p;
+        p->next = r->next;
+        p->prev = r;
+        r->next = p;
+    }
+}
+
+void del_node(list *&h, list *&t, list *&r) {
+    if (r == h && r == t) {
+        h = t = nullptr;
+    }
+    else if (r == h) {
+        h = h->next;
+        h->prev = nullptr;
+    }
+    else if (r == t) {
+        t = t->prev;
+        t->next = nullptr;
+    }
+    else {
+        r->next->prev = r->prev;
+        r->prev->next = r->next;
+    }
+    delete r;
+}
+
+void del_list(list *&h, list *&t) {
+    while(h) {
+        list *p = h;
+        h = h->next;
+        h->prev = nullptr;
+        delete p;
+    }
+}
+
+void res(list *&h, list *&t, int n) {
+    list *temp = h;
+    int i = 0;
+    while(i < n) {
+        if (temp->inf % 2 == 0) {
+            insert_after(h, t, t, temp->inf);
+            del_node(h, t, temp);
+        }
+        temp = temp->next;
+        i++;
+    }
+    print(h, t);
+}
+
+int main() {
+    
+}
