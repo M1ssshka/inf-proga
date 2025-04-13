7f383939f7de0b36f42a75c08bbf02762f59351d
Mikhail Slepov <anim66056@gmail.com>
2025-04-13 10:27:10 +0400

    chore(init): initialize timsort skeleton     - Added basic headers and function prototypes     - Created empty compute_minrun() and insertion_sort()

---
+++ teamsort/teamsort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int compute_minrun(int n) {
+    
+}
+
+void insertion_sort(vector<int> &a) {
+
+}
+
+vector<int> merge(vector<int> &a, vector<int> &b) {
+    
+}
+
+vector<int> timsort(vector<int> a) {
+    
+}
+
+int main() {
+
+}
