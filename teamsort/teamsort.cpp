a5ec93d122a92e8edd1f78022944a89b9bb65f67
Mikhail Slepov <anim66056@gmail.com>
2025-04-13 10:28:39 +0400

    feat(algorithm): implemented minrun calculation     - Added bitwise minrun computation

--- teamsort/teamsort.cpp
+++ teamsort/teamsort.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 int compute_minrun(int n) {
-    
+    int r = 0;
+    while(n >= 64) {
+        r |= n & 1;
+        n >>= 1;
+    }
+    return n + r;
 }
 
 void insertion_sort(vector<int> &a) {
