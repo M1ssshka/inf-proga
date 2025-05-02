303bd48301bac8f51c1af98584bbf53319945c38
Mikhail Slepov <anim66056@gmail.com>
2025-05-02 16:31:59 +0400

    feat: added pretty-print tree visualization function

--- binary search trees/task1.cpp
+++ binary search trees/task1.cpp
@@ -59,12 +59,14 @@ void insert(tree *&tr, int x)
         }
     }
 }
-tree *find(tree *tr, int x) {
-    if (!tr || x == tr->inf) 
+
+tree *find(tree *tr, int x)
+{
+    if (!tr || x == tr->inf)
         return tr;
-    if (x < tr->inf) 
+    if (x < tr->inf)
         return find(tr->left, x);
-    else 
+    else
         return find(tr->right, x);
 }
 
@@ -80,6 +82,60 @@ int heightNode(tree *tr)
     }
 }
 
-int main() {
+void print_tree(tree *tr, int k)
+{
+    int n = k;
+    for (int i = 0; i < (int)pow(2.0, n + 1) - 1; i++)
+        cout << '*';
+    cout << '\n';
+    if (!tr)
+        cout << "tree is empty" << endl;
+    else
+    {
+        queue<tree *> cur, next;
+        tree *r = tr;
+        cur.push(r);
+        int j = 0;
+        while (cur.size())
+        {
+            if (j == 0)
+            {
+                for (int i = 0; i < (int)pow(2.0, k) - 1; i++)
+                    cout << ' ';
+            }
+            tree *buf = cur.front();
+            cur.pop();
+            j++;
+            if (buf)
+            {
+                cout << buf->inf;
+                next.push(buf->left);
+                next.push(buf->right);
+                for (int i = 0; i < (int)pow(2.0, k + 1) - 1; i++)
+                    cout << ' ';
+            }
 
+            if (!buf)
+            {
+                for (int i = 0; i < (int)pow(2.0, k + 1) - 1; i++)
+                    cout << ' ';
+                cout << ' ';
+            }
+
+            if (cur.empty())
+            {
+                cout << endl;
+                swap(cur, next);
+                j = 0;
+                k--;
+            }
+        }
+    }
+    for (int i = 0; i < (int)pow(2.0, n + 1) - 1; i++)
+        cout << '*';
+    cout << '\n';
+}
+
+int main()
+{
 }
