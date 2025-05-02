3fb654cb391c8eff26cac9701040a39a914f9610
Mikhail Slepov <anim66056@gmail.com>
2025-05-02 16:24:20 +0400

    feat: added binary tree implementation with basic operations

---
+++ binary search trees/task1.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <math.h>
+using namespace std;
+
+struct tree
+{
+    int inf;
+    tree *right;
+    tree *left;
+    tree *parent;
+};
+
+tree *node(int x)
+{
+    tree *n = new tree;
+    n->inf = x;
+    n->left = n->right = NULL;
+    n->parent = NULL;
+    return n;
+}
+
+void insert(tree *&tr, int x)
+{
+    tree *n = node(x);
+    if (!tr)
+        tr = n;
+    else
+    {
+        tree *y = tr;
+        while (y)
+        {
+            if (n->inf > y->inf)
+            {
+                if (y->right)
+                {
+                    y = y->right;
+                }
+                else
+                {
+                    n->parent = y;
+                    y->right = n;
+                    break;
+                }
+            }
+            else if (n->inf < y->inf)
+            {
+                if (y->left)
+                {
+                    y = y->left;
+                }
+                else
+                {
+                    n->parent = y;
+                    y->left = n;
+                    break;
+                }
+            }
+        }
+    }
+}
+tree *find(tree *tr, int x) {
+    if (!tr || x == tr->inf) 
+        return tr;
+    if (x < tr->inf) 
+        return find(tr->left, x);
+    else 
+        return find(tr->right, x);
+}
+
+int main() {
+    
+}
