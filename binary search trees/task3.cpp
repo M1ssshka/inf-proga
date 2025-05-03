4cd8cf78a666bef64acfd066e6e485d46458165e
Mikhail Slepov <anim66056@gmail.com>
2025-05-03 11:23:47 +0400

    feat: added tree printing function

--- binary search trees/task3.cpp
+++ binary search trees/task3.cpp
@@ -113,6 +113,60 @@ tree *Next(tree *tr, int x)
     return y;
 }
 
+void print_tree(tree *tr, int k)
+{
+    int n = k;
+    for (int i = 0; i < (int)pow(2.0, n + 1) - 1; i++)
+        cout << '*';
+    cout << '\n';
+    if (!tr)
+        cout << "tree is empty" << endl;
+    else
+    {
+        queue<tree *> cur, next;
+        tree *r = tr;
+        cur.push(r);
+        int j = 0;
+        while (cur.size())
+        {
+            if (j == 0)
+            {
+                for (int i = 0; i < (int)pow(2.0, k) - 1; i++)
+                    cout << ' ';
+            }
+            tree *buf = cur.front();
+            cur.pop();
+            j++;
+            if (buf)
+            {
+                cout << buf->inf;
+                next.push(buf->left);
+                next.push(buf->right);
+                for (int i = 0; i < (int)pow(2.0, k + 1) - 1; i++)
+                    cout << ' ';
+            }
+
+            if (!buf)
+            {
+                for (int i = 0; i < (int)pow(2.0, k + 1) - 1; i++)
+                    cout << ' ';
+                cout << ' ';
+            }
+
+            if (cur.empty())
+            {
+                cout << endl;
+                swap(cur, next);
+                j = 0;
+                k--;
+            }
+        }
+    }
+    for (int i = 0; i < (int)pow(2.0, n + 1) - 1; i++)
+        cout << '*';
+    cout << '\n';
+}
+
 int main()
 {
 }
