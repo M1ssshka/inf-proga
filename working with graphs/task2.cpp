944511c5805a96d3d501aa54538d4382ddf6eba0
Mikhail Slepov <anim66056@gmail.com>
2025-05-06 10:25:21 +0400

    feat: added edge insertion between vertices A and B

--- working with graphs/task2.cpp
+++ working with graphs/task2.cpp
@@ -19,4 +19,26 @@ int main()
         gr[v].push_back(u);
         gr[u].push_back(v);
     }
+    int A, B;
+    cin >> A >> B;
+    for (int i = 0; i < gr.size(); i++)
+    {
+        cout << i + 1 << " : ";
+        for (int j : gr[i])
+        {
+            cout << j + 1 << ' ';
+        }
+        cout << '\n';
+    }
+    if (A > n || B > n) {
+        if (A > n)
+            gr.push_back({});
+        if (B > n)
+            gr.push_back({});
+    }
+    A--;
+    B--;
+    cout << '\n';
+    gr[A].push_back(B);
+    gr[B].push_back(A);
 }
