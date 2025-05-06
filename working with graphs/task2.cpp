8ee8c65191b3df5879f67c634038e2536b3b10ca
Mikhail Slepov <anim66056@gmail.com>
2025-05-06 10:24:26 +0400

    feat: added initial graph implementation with adjacency list

---
+++ working with graphs/task2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> gr;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    gr.assign(n, {});
+    for (int i = 0; i < m; i++)
+    {
+        int v, u;
+        cin >> v >> u;
+        v--;
+        u--;
+        gr[v].push_back(u);
+        gr[u].push_back(v);
+    }
+}
