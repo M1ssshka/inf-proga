3fb2a72ab26da901165db3ebb143698f0159ffe0
Mikhail Slepov <anim66056@gmail.com>
2025-05-06 10:36:15 +0400

    feat: implemented algorithm for SCC detection

--- working with graphs/task4.cpp
+++ working with graphs/task4.cpp
@@ -4,8 +4,22 @@
 using namespace std;
 
 vector<vector<int>> gr;
+vector<vector<int>> tgr;
 vector<char> used;
 vector<int> ord;
+
+vector<int> clr;
+int comp = 0;
+
+void dfs(int v)
+{
+    clr[v] = comp;
+    for (int u : tgr[v])
+    {
+        if (clr[u] == -1)
+            dfs(u);
+    }
+}
 
 void ts(int v) {
     used[v] = true;
@@ -20,6 +34,7 @@ int main()
     int n, m;
     cin >> n >> m;
     gr.assign(n, {});
+    tgr.assign(n, {});
     for (int i = 0; i < m; i++)
     {
         int v, u;
@@ -27,6 +42,7 @@ int main()
         v--;
         u--;
         gr[v].push_back(u);
+        tgr[u].push_back(v);
     }
     used.assign(n, {});
     for (int i = 0; i < n; i++)
@@ -34,4 +50,23 @@ int main()
         if (!used[i]) ts(i);
     }
     reverse(ord.begin(), ord.end());
+    clr.assign(n, -1);
+    for (int i : ord)
+    {
+        if (clr[i] == -1)
+        {
+            dfs(i);
+            comp++;
+        }
+    }
+    cout << '\n';
+    int t = 0;
+    for (int x : ord) {
+        if (t != clr[x]) {
+            cout << '\n';
+            t = clr[x];
+        }
+        cout << x + 1 << ' ';
+    }
+    cout << '\n';
 }
