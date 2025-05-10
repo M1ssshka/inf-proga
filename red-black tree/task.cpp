995623d25ca06b45154d2f8d9b8d643fe1050bfb
Mikhail Slepov <anim66056@gmail.com>
2025-05-10 16:23:54 +0400

    feat: implemented helper functions (grandparent, uncle, sibling)

--- red-black tree/task.cpp
+++ red-black tree/task.cpp
@@ -30,6 +30,34 @@ tree *root(int x) {
     return n;
 }
 
+tree *grandparent(tree *x) {
+    if (x && x->parent)
+        return x->parent->parent;
+    else
+        return nullptr;
+}
+
+tree *uncle(tree *x) {
+    tree *g = grandparent(x);
+    if (!g)
+        return nullptr;
+    if (x->parent == g->left)
+        return g->right;
+    else
+        return g->left;
+}
+
+tree *sibling(tree *x) {
+    if (x && x->parent) {
+        if (x == x->parent->left)
+            return x->parent->right;
+        else
+            return x->parent->left;
+    }
+    else
+        return nullptr;
+}
+
 int main() {
-    
+
 }
