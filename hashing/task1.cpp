85e6134bfbbf6c18382edfe5fe210120463b29a4
Mikhail Slepov <anim66056@gmail.com>
2025-05-14 12:15:32 +0400

    feat: implemented basic operations with doubly linked list (push, print)

--- hashing/task1.cpp
+++ hashing/task1.cpp
@@ -28,3 +28,34 @@ struct List {
     list *h;
     list *t;
 };
+
+void push(list *&h, list *&t, worker x) {
+    list *r = new list;
+    r->inf = x;
+    r->next = nullptr;
+    if (!h && !t) {
+        r->prev = nullptr;
+        h = r;
+    }
+    else {
+        t->next = r;
+        r->prev = t;
+    }
+    t = r;
+}
+
+void print_worker(worker human) {
+    string i = human.date_of_birthday.day / 10 == 0 ? "0" : "";
+    string i1 = human.date_of_birthday.month / 10 == 0 ? "0" : "";
+    cout << human.surname << ", " << i << human.date_of_birthday.day << "." << i1 << human.date_of_birthday.month << "."
+        << human.date_of_birthday.year << ", " << human.experience << ", " << human.post << ", " << human.pay << endl;
+}
+
+void print(list *&h, list *&t) {
+    list *p = h;
+    while(p) {
+        print_worker(p->inf);
+        p = p->next;
+    }
+    cout << '\n';
+}
