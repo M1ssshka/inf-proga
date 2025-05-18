e3526414e9eb70dd16dd1624610850f3e736b5bd
M1ssshka <anim66056@gmail.com>
2025-05-18 21:39:13 +0400

    feat: implemented a function to convert a string to a worker

--- hashing/task2.cpp
+++ hashing/task2.cpp
@@ -17,3 +17,23 @@ struct worker {
     int pay;
 };
 
+
+worker str_to_worker(string str) {
+    worker human;
+    size_t k = 0; size_t k1 = str.find_first_of(',');
+    human.surname = str.substr(k, k1);
+    k = k1 + 2; k1 = str.find_first_of(',', k);
+    human.experience = stoi(str.substr(k, k1 - k));
+    k = k1 + 2; k1 = str.find_first_of(',', k);
+    size_t i = k; size_t i1 = str.find_first_of('.', k);
+    human.date_of_birthday.day = stoi(str.substr(i, i1 - i));
+    i = i1 + 1; i1 = str.find_first_of('.', i);
+    human.date_of_birthday.month = stoi(str.substr(i, i1 - i));
+    i = i1 + 1; i1 = str.find_first_of('.', i);
+    human.date_of_birthday.year = stoi(str.substr(i, i1 - i));
+    k = k1 + 2; k1 = str.find_first_of(',', k);
+    human.post = str.substr(k, k1 - k);
+    k = k1 + 2; k1 = str.find_first_of(',', k);
+    human.pay = stoi(str.substr(k, k1 - k));
+    return human;
+}
