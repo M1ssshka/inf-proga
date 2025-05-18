aa521c8146e15895a41fe5f6381992469203db7b
M1ssshka <anim66056@gmail.com>
2025-05-18 21:35:36 +0400

    feat: added date and worker data structures

---
+++ hashing/task2.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct date {
+    int day;
+    int month;
+    int year;
+};
+
+struct worker {
+    string post;
+    string surname;
+    date date_of_birthday;
+    int experience;
+    int pay;
+};
+
